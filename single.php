e9d2aa36b5e98beaf241892fd65e7af596e00f64
iamalleksy <Plague1995>
2016-10-07 01:40:40 +0300

    add single page template

---
+++ single.php
@@ -0,0 +1,34 @@
+<?php get_header(); ?>
+<body>
+    <main class="content">
+
+        <?php get_sidebar(); ?>
+
+            <?php the_post(); ?>
+
+            <div class="inner">
+                <div class="post-body">
+                    <div class="post-text">
+                        <h3 class="post-header"><?php the_title(); ?></h3>
+
+                        <?php the_content(); ?>
+
+                    </div>
+
+                    <div class="post-img">
+                        <div class="fix">
+                            <?php echo get_the_post_thumbnail( $id, 'medium', array('class' => 'wp-post-image') ); ?>
+
+                            <p class="img-header">
+
+                            </p>
+                            
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        <?php get_footer(); ?>
+
+    </main>
