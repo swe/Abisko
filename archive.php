d85b471554c26b6f8d8a28912ef7a35ab501f408
iamalleksy <Plague1995>
2016-10-07 01:37:36 +0300

    add archive & archive loop

---
+++ archive.php
@@ -0,0 +1,24 @@
+<?php
+
+/* Template Name: Archive Page */
+
+get_header(); ?>
+
+<main class="content">
+
+<?php get_sidebar(); ?>
+
+    <div class="inner">
+
+        <div class="posts">
+
+            <h3 class="blog-post">Blog</h3>
+
+            <?php get_template_part('loop-archive'); ?>
+
+        </div>
+    </div>
+
+    <?php get_footer(); ?>
+
+</main>
