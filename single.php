cd26a0f4ec696f62d786e63f35cbb9a91ba5db86
swe <johnnywtf@yandex.ru>
2016-10-07 04:21:34 +0300

    add  'back to blog' button

--- single.php
+++ single.php
@@ -1,34 +1,38 @@
 <?php get_header(); ?>
 <body>
-    <main class="content">
+<main class="content">
 
-        <?php get_sidebar(); ?>
+    <?php get_sidebar(); ?>
 
-            <?php the_post(); ?>
+    <?php the_post(); ?>
 
-            <div class="inner">
-                <div class="post-body">
-                    <div class="post-text">
-                        <h3 class="post-header"><?php the_title(); ?></h3>
+    <div class="inner">
+        <div class="post-body">
+            <div class="post-text">
+                <h3 class="post-header"><?php the_title(); ?></h3>
 
-                        <?php the_content(); ?>
+                <?php the_content(); ?>
 
-                    </div>
+            </div>
 
-                    <div class="post-img">
-                        <div class="fix">
-                            <?php echo get_the_post_thumbnail( $id, 'medium', array('class' => 'wp-post-image') ); ?>
+            <div class="post-img">
+                <div class="fix">
+                    <?php echo get_the_post_thumbnail( $id, 'medium', array('class' => 'wp-post-image') ); ?>
 
-                            <p class="img-header">
+                    <p class="img-header">
 
-                            </p>
-                            
-                        </div>
-                    </div>
+                    </p>
 
                 </div>
             </div>
 
-        <?php get_footer(); ?>
+        </div>
 
-    </main>
+        <div class="post-footer">
+            <a href="../../blog" class="button">Back to all posts</a>
+        </div>
+    </div>
+
+    <?php get_footer(); ?>
+
+</main>
