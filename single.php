2393b4a45158803e329c6c5fbfcbfe1be0ddea85
swe <johnnywtf@yandex.ru>
2016-10-10 22:18:50 +0300

    update single page

--- single.php
+++ single.php
@@ -1,38 +1,21 @@
-<?php get_header(); ?>
+<?php get_header(); get_template_part( 'partials/mytemplate' ); ?>
+
 <body>
 <main class="content">
 
     <?php get_sidebar(); ?>
 
-    <?php the_post(); ?>
-
     <div class="inner">
-        <div class="post-body">
-            <div class="post-text">
-                <h3 class="post-header"><?php the_title(); ?></h3>
 
-                <?php the_content(); ?>
+        <div class="posts">
+            <?php the_post(); ?>
+            <?php the_title( '<h1>', '</h1>' ); ?>
+            <?php the_content(); ?>
 
-            </div>
-
-            <div class="post-img">
-                <div class="fix">
-                    <?php echo get_the_post_thumbnail( $id, 'medium', array('class' => 'wp-post-image') ); ?>
-
-                    <p class="img-header">
-
-                    </p>
-
-                </div>
-            </div>
-
-        </div>
-
-        <div class="post-footer">
-            <a href="../../blog" class="button">Back to all posts</a>
         </div>
     </div>
 
     <?php get_footer(); ?>
 
 </main>
+
