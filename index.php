173f1796c60127f7618a335696939ccff051f44d
swe <johnnywtf@yandex.ru>
2016-10-10 22:33:39 +0300

    last index update

--- index.php
+++ index.php
@@ -13,24 +13,17 @@
             <div class="intro">
                 <p>
                     Hello — I am a dreamer, web developer, designer, and musician.
-                    My home is in Stockholm, Sweden, but I live in awesome
-                    Moscow, Russia with my girlfriend. This site contains some of my
+                    My home is in Stockholm, Sweden. This site contains some of my
                     recent work and writing — primarily focused on coding and design,
                     or movies and music.
                 </p>
                 <p>
-                    I work with great people in <a href="http://drinkmilkdesign.com">Drink Milk! Studio</a>
-                    as the main web developer. More than coffee I love to listen to music. You can find me
-                    and my playlists on <a href="http://instagram.com/iamalleksy">Spotify</a>. Sometimes I
-                    post photos to <a href="http://instagram.com/iamalleksy">Instagram</a>, some short
-                    thoughts to <a href="https://twitter.com/iamalleksy">Twitter</a>, sample code to
-                    <a href="https://github.com/swe">GitHub</a>.
-                    <!-- and some design works to <a href="https://www.behance.net/iamalleksy">Behance</a>-->
+                    If you want to change this text, go to Appearance -> Editor -> Main Index Template and change all you want.
                 </p>
                 <p>
-                    Thanks for visiting!
+                    Or you can create new page and make it index.
                 </p>
-                <img src="<?php echo get_template_directory_uri(); ?>/img/sign.png">
+
             </div>
             <div class="posts">
                 <h3>Recent posts</h3>
