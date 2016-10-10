61178c1e0f3b4e777cc0477a1dcce25a7afb2299
swe <johnnywtf@yandex.ru>
2016-10-10 22:19:46 +0300

    update single page

--- page.php
+++ page.php
@@ -5,17 +5,14 @@
 
     <?php get_sidebar(); ?>
 
-    <?php the_post(); ?>
-
     <div class="inner">
-        <?php if (have_posts()): while (have_posts()): the_post(); ?>
 
-                <?php the_title( '<h1>', '</h1>' ); ?>
-                <?php the_content(); ?>
+        <div class="posts">
+            <?php the_post(); ?>
+            <?php the_title( '<h1>', '</h1>' ); ?>
+            <?php the_content(); ?>
 
-        <?php endwhile; endif; ?>
-        <?php get_sidebar(); ?>
-        <?php comments_template(); ?>
+        </div>
     </div>
 
     <?php get_footer(); ?>
